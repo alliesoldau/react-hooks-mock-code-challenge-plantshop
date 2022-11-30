aca4cc2f3a2480ff285485b3bbb103fa28b970ed
Allie Soldau <alliesoldau@gmail.com>
2022-11-30 10:48:56 -0500

    you can update the price

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -3,9 +3,25 @@ import React, { useState } from "react";
 function PlantCard({ plant }) {
 
   const [stockStatus, setStockStatus] = useState(true)
+  const [newPrice, setNewPrice] = useState(0)
 
   function handleStockStatus() {
     setStockStatus(!stockStatus);
+  }
+
+  function handleNewPrice() {
+    const parsedPrice = parseInt(newPrice, 10)
+    fetch(`http://localhost:6001/plants/${plant.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        price: parsedPrice
+      }),
+    })
+    .then((r) => r.json())
+    .then((data) => console.log(data))
   }
 
   return (
@@ -20,6 +36,22 @@ function PlantCard({ plant }) {
           <button>Out of Stock</button>
         )}
       </div>
+      <form onSubmit={handleNewPrice}>
+        <h4> Update Price: </h4>
+          <input 
+            type="number" 
+            name="newPrice"
+            placeholder="$0"
+            value={newPrice}
+            onChange={(e) => setNewPrice(e.target.value)}
+          />
+          <input
+              type="submit"
+              name="submit"
+              value="Update Price"
+              className="submit"
+          />
+      </form>
     </li>
   );
 }
