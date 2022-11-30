efd616abb411478fcb3b65adaaf5c899b3e7518f
Allie Soldau <alliesoldau@gmail.com>
2022-11-30 10:08:43 -0500

    plants render upon load

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,20 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plantData }) {
+
+  // console.log(plantData)
+  const plantCards = plantData.map((plant) => {
+    return (
+      <PlantCard
+        key={plant.id}
+        plant={plant}
+      />
+    )
+  })
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plantCards}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plantData, setPlantData] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((r) => r.json())
+    .then((data) => setPlantData(data))
+  }, [])
+
+  // console.log(plantData)
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList 
+        plantData={plantData}
+      />
     </main>
   );
 }
